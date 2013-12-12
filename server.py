74b1e555bca22f3f9d31287c9e2c6d288da43e2d
Shao-Ping Lee <slee168@illinois.edu>
2013-12-12 16:49:02 -0600

    Added a server script to view the list on a browser

---
+++ server.py
@@ -0,0 +1,37 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import os
+from LLog import LLog
+
+class SLHTTPRequestHandler(BaseHTTPRequestHandler):
+    # Handle GET
+    def do_GET(self):
+        log = LLog('japn201vocab.csv')
+        try:
+            # Send 200 response
+            self.send_response(200)
+
+            # Send header
+            self.send_header('Content-type','text-html')
+            self.end_headers()
+
+            # Send Content
+            self.wfile.write(str(log))
+            return
+        except IOError:
+            self.send_error(404, 'file not found')
+
+def run():
+    try:
+        print("Starting server...")
+        server_address = ('127.0.0.1',8080)
+        httpd = HTTPServer(server_address, SLHTTPRequestHandler)
+        print("Server is running...")
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print('^C received, shutting down the server')
+        httpd.socket.close()
+
+if __name__ == "__main__":
+    run()
+
+
